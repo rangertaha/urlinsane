200ca610a418cb5b4473f4d632cd315692515fba
Tal Hachi <talhachi2019@gmail.com>
2019-06-02 21:08:26 -0400

    Updates

--- pkg/typo/levenshtein.go
+++ pkg/typo/levenshtein.go
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright © 2018 rangertaha rangertaha@gmail.com
+// Copyright © 2018 Tal Hachi
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
