2d2451d9bf88b649279cfae78ac1928428286b2f
Rangertaha <rangertaha@gmail.com>
2024-07-13 17:21:42 -0400

    cleaning up code and making it simpler

---
+++ plugins/information/none/none.go
@@ -0,0 +1,41 @@
+package none
+
+import (
+	typo "github.com/rangertaha/urlinsane"
+	"github.com/rangertaha/urlinsane/plugins/information"
+)
+
+type None struct {
+	// Code() string
+	// Name() string
+	// Description() string
+	// Fields() []string
+	// Exec() func(Result) []Result
+}
+
+func (n *None) Code() string {
+	return ""
+}
+
+func (n *None) Name() string {
+	return "None"
+}
+
+func (n *None) Description() string {
+	return ""
+}
+
+func (n *None) Fields() []string {
+	return []string{}
+}
+
+func (n *None) Exec(typo.Result) (results []typo.Result) {
+	return
+}
+
+// Register the plugin
+func init() {
+	information.Add("none", func() typo.Module {
+		return &None{}
+	})
+}
