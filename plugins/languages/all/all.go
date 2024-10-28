cd929b9565ef52301f00d0a028dcdfeb7633949d
Rangertaha <rangertaha@gmail.com>
2024-10-28 13:54:38 -0400

    updates

---
+++ plugins/languages/all/all.go
@@ -0,0 +1,14 @@
+package all
+
+import (
+	// registered plugin
+	_ "github.com/rangertaha/urlinsane/plugins/languages/arabic"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/armenian"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/english"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/finnish"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/french"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/hebrew"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/persian"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/russian"
+	_ "github.com/rangertaha/urlinsane/plugins/languages/spanish"
+)
