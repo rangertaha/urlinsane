850b4286ddcc14fea3692f6639d8d2b95ade2324
Rangertaha <rangertaha@gmail.com>
2024-11-06 07:32:34 -0500

    updates

--- internal/plugins/outputs/html/html.go
+++ internal/plugins/outputs/html/html.go
@@ -49,14 +49,17 @@ func (n *Text) Init(conf internal.Config) {
 	n.table.AppendHeader(n.Header())
 }
 
+
 func (n *Text) Header() (row table.Row) {
 	row = append(row, "LD")
 	row = append(row, "TYPE")
 	row = append(row, "TYPO")
 
 	for _, info := range n.config.Information() {
-		for _, headers := range info.Headers() {
-			row = append(row, headers)
+		for _, header := range info.Headers() {
+			if n.Filter(header) {
+				row = append(row, header)
+			}
 		}
 	}
 	return
@@ -73,15 +76,30 @@ func (n *Text) Row(typo internal.Typo) (row table.Row) {
 
 	for _, info := range n.config.Information() {
 		for _, header := range info.Headers() {
-			meta := typo.Variant().Meta()
-			if col, ok := meta[header]; ok {
-				row = append(row, col)
-			} else {
-				row = append(row, "")
+			if n.Filter(header) {
+				meta := typo.Variant().Meta()
+				if col, ok := meta[header]; ok {
+					row = append(row, col)
+				} else {
+					row = append(row, "")
+				}
 			}
 		}
 	}
 	return
+}
+
+func (n *Text) Filter(header string) bool {
+	header = strings.TrimSpace(header)
+	header = strings.ToLower(header)
+	for _, filter := range n.config.Filters() {
+		filter = strings.TrimSpace(filter)
+		filter = strings.ToLower(filter)
+		if filter == header {
+			return true
+		}
+	}
+	return false
 }
 
 func (n *Text) Progress(typo <-chan internal.Typo) <-chan internal.Typo {
