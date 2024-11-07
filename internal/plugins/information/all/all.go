7e7d76b242837cc5edf9bfc1ff6148a040273206
Rangertaha <rangertaha@gmail.com>
2024-11-07 09:12:12 -0500

    updats

--- internal/plugins/information/all/all.go
+++ internal/plugins/information/all/all.go
@@ -3,6 +3,4 @@ package all
 // register all plugins
 import (
 	_ "github.com/rangertaha/urlinsane/internal/plugins/information/domains/all"
-	_ "github.com/rangertaha/urlinsane/internal/plugins/information/packages/all"
-	_ "github.com/rangertaha/urlinsane/internal/plugins/information/usernames/all"
 )
