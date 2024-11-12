8de0f59999622b6f5332b131014476ed36c25321
Rangertaha <rangertaha@gmail.com>
2024-11-11 22:09:43 -0500

    updates

--- internal/plugins/collectors/all/all.go
+++ internal/plugins/collectors/all/all.go
@@ -5,8 +5,10 @@ import (
 	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/cn"
 	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/geo"
 	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/idn"
+	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/img"
 	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/ip"
 	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/mx"
 	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/ns"
 	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/txt"
+	_ "github.com/rangertaha/urlinsane/internal/plugins/collectors/web"
 )
