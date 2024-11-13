aa011fb508299f9a0d42c3f2d56531bc5435a827
Rangertaha <rangertaha@gmail.com>
2024-11-13 10:37:58 -0500

    updates

---
+++ internal/plugins/collectors/wi/wi.go
@@ -0,0 +1,31 @@
+// Copyright (C) 2024 Rangertaha
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package wi
+
+import (
+	"github.com/rangertaha/urlinsane/internal"
+	// 	log "github.com/sirupsen/logrus"
+)
+
+type Plugin struct{}
+
+func (i *Plugin) Init(c internal.Config) {
+
+}
+
+func (i *Plugin) Exec(acc internal.Accumulator) (err error) {
+
+	return
+}
