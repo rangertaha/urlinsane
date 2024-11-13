c62a7d600eda32738259f1367da82ff7821a5883
Rangertaha <rangertaha@gmail.com>
2024-11-13 11:24:07 -0500

    updates

--- scripts/geoip.sh
+++ scripts/geoip.sh
@@ -6,7 +6,7 @@ FILE="internal/plugins/collectors/geo/GeoLite2-City.mmdb"
 # # https://git.io/GeoLite2-City.mmdb
 # # https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb
 
-# wget "https://git.io/GeoLite2-City.mmdb" -P internal/plugins/collectors/geo
+wget "https://git.io/GeoLite2-City.mmdb" -P internal/plugins/collectors/geo
 
 if [ -f ! $FILE ]; then
    wget "https://git.io/GeoLite2-City.mmdb" -P internal/plugins/collectors/geo
