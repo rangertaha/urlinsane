fa0e03e31761d257d4fb42c7ec77e43830a0f4a2
Rangertaha <rangertaha@gmail.com>
2024-11-18 17:36:43 -0500

    Update README.md

---
+++ internal/plugins/algorithms/cb/cb.go
@@ -0,0 +1,60 @@
+// Copyright 2024 Rangertaha. All Rights Reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package cb
+
+import (
+	"github.com/rangertaha/urlinsane/internal"
+	"github.com/rangertaha/urlinsane/internal/plugins/algorithms"
+)
+
+const (
+	CODE        = "com"
+	NAME        = "Combo Squatting"
+	DESCRIPTION = "Creating domain names by combining a legitimate brand or word with another keyword"
+)
+
+type Algo struct {
+	config    internal.Config
+	languages []internal.Language
+	keyboards []internal.Keyboard
+}
+
+func (n *Algo) Id() string {
+	return CODE
+}
+
+func (n *Algo) Init(conf internal.Config) {
+	n.keyboards = conf.Keyboards()
+	n.languages = conf.Languages()
+	n.config = conf
+}
+
+func (n *Algo) Name() string {
+	return NAME
+}
+func (n *Algo) Description() string {
+	return DESCRIPTION
+}
+
+func (n *Algo) Exec(original internal.Domain, acc internal.Accumulator) (err error) {
+	return
+}
+
+// Register the plugin
+func init() {
+	algorithms.Add(CODE, func() internal.Algorithm {
+		return &Algo{}
+	})
+}
