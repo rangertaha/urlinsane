b95675e90feeab2cf7202c00823534f32ae3cbaa
Rangertaha <rangertaha@gmail.com>
2024-11-23 10:26:50 -0500

    updates

---
+++ scripts/mmdb.sh
@@ -0,0 +1,22 @@
+
+
+
+FILE="internal/plugins/collectors/geo/GeoLite2-Country.mmdb"
+
+# # https://git.io/GeoLite2-City.mmdb
+# # https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb
+
+wget "https://git.io/GeoLite2-Country.mmdb" -P internal/plugins/collectors/geo
+
+if [ -f ! $FILE ]; then
+   wget "https://git.io/GeoLite2-Country.mmdb" -P internal/plugins/collectors/geo
+else
+   echo "File $FILE exists"
+fi
+
+if [ -f ! $FILE ]; then
+   wget "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-Country.mmdb" -P internal/plugins/collectors/geo
+else
+   echo "File $FILE exists"
+fi
+
